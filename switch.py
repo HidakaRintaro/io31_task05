416ae2448263c307dfe75ee312fe2496a7de8f9b
Rintaro raspi <53255166+HidakaRintaro@users.noreply.github.com>
2021-11-11 10:24:25 +0900

    制御っぽいプログラムに変更

--- switch.py
+++ switch.py
@@ -4,9 +4,11 @@ import time
 SWPIN = 17
 LEDPIN1 = 27
 LEDPIN2 = 22
+
 SWINPUT = 1
-STACK = 1
-FLAG = False
+BEFORE = 1
+MOMFLG = False
+ALTFLG = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SWPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # プルアップ抵抗は常時1、ボタン押下時0
@@ -20,25 +22,24 @@ GPIO.setup(LEDPIN2, GPIO.OUT)
 # オルタネート
 # 立ち上がりエッジ(LOW→HIGH)のときにalternateを実行し300ミリ秒待ってから次のイベントを呼ぶ
 # GPIO.add_event_detect(SWPIN, GPIO.RISING, callback=alternate, bouncetime=300)
-
+CNT = 0
 try:
     while True:
         SWINPUT = GPIO.input(SWPIN)
-        
+        MOMFLG = not SWINPUT
+
         # オルタネート処理
-        if SWINPUT == 0 and STACK == 1:
-            FLAG = not FLAG
+        if SWINPUT == 0 and BEFORE == 1:
+            ALTFLG = not ALTFLG
         
-        STACK = SWINPUT
+        # 1つ前の入力状態を保持する
+        BEFORE = SWINPUT
         
         # モーメンタリ出力
-        if SWINPUT == 0:
-            GPIO.output(LEDPIN1, GPIO.HIGH)
-        else:
-            GPIO.output(LEDPIN1, GPIO.LOW)
+        GPIO.output(LEDPIN1, MOMFLG)
         
         # オルタネート出力
-        GPIO.output(LEDPIN2, FLAG)
+        GPIO.output(LEDPIN2, ALTFLG)
         
 except KeyboardInterrupt:
     pass
